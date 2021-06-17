47e2b2d8b4fabb44b32ec6cb0c678b9edf4a27b7
Jailbreak Updates <jailbreak14update@gmail.com>
2021-06-17 08:31:45 -0500

    add alert

---
+++ Con Computadora/js/sweetalert.js
@@ -0,0 +1,14 @@
+Swal.fire({
+	html: '<strong><h3>Esta seccion es EXCLUSIVA PARA PC, MAC O Linux',
+	position: "top",
+	allowOutsideClick: false,
+	allowEscapeKey: false,
+	allowEnterKey: false,
+	showConfirmButton: true,
+	showClass: {
+	  popup: 'animate__animated animate__fadeInDown'
+	},
+	hideClass: {
+	  popup: 'animate__animated animate__fadeOutUp'
+	}
+  })
---
+++ Sin-computadora/js/sweetalert.js
@@ -0,0 +1,14 @@
+Swal.fire({
+	html: '<strong><h3>Esta seccion es EXCLUSIVA PARA iOS</h3></strong>',
+	position: "top",
+	allowOutsideClick: false,
+	allowEscapeKey: false,
+	allowEnterKey: false,
+	showConfirmButton: true,
+	showClass: {
+	  popup: 'animate__animated animate__fadeInDown'
+	},
+	hideClass: {
+	  popup: 'animate__animated animate__fadeOutUp'
+	}
+  })
