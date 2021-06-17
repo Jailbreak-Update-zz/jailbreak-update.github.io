c3665865d3ae78e37bc7f7ea6b87ab6b25a71889
Jailbreak Updates <jailbreak14update@gmail.com>
2021-06-16 19:24:42 -0500

    update arlet

---
+++ js/sweetalert.js
@@ -0,0 +1,17 @@
+Swal.fire({
+	text: "La versión en inglés está en proceso de renovación. (The English version is under renovation)",
+	icon: "info",
+	timer: 6000,
+	timerProgressBar: true,
+	position: "top",
+	allowOutsideClick: false,
+	allowEscapeKey: false,
+	allowEnterKey: false,
+	showConfirmButton: false,
+	showClass: {
+	  popup: 'animate__animated animate__fadeInDown'
+	},
+	hideClass: {
+	  popup: 'animate__animated animate__fadeOutUp'
+	}
+  })
