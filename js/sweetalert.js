eed460c2a6f9ce697a230462312a6909269cfd9e
Jailbreak Updates <jailbreak14update@gmail.com>
2021-07-03 12:39:32 -0500

    update JBU

--- js/sweetalert.js
+++ js/sweetalert.js
@@ -14,4 +14,54 @@ Swal.fire({
 	hideClass: {
 	  popup: 'animate__animated animate__fadeOutUp'
 	}
-  })
+  });
+
+
+  $("#btn0").click(function(){
+    const { value: idioma } = Swal.fire({
+		allowOutsideClick: false,
+		allowEscapeKey: false,
+		allowEnterKey: false,
+	  confirmButtonText: 'Seleccionar',
+	  backdrop: true,
+		html: '<h2>Seleciona tu idioma</h2> <h3><p>(Select your language)</p></h3>',
+		input: 'select',
+		inputOptions: {
+			Español: 'Español (Spanish)',
+			Ingles: 'Ingles (English)'
+		},
+		inputValidator: (value) => {
+			return new Promise((resolve) => {
+			  if (value === 'Español') {
+				Swal.fire({
+				  allowOutsideClick: false,
+					 allowEscapeKey: false,
+					allowEnterKey: false,
+				  showConfirmButton: true,
+				  confirmButtonText: '<a href="https://jailbreak-update.github.io/" style="text-decoration: none; color: white;">Si</a>',
+				  showCancelButton: true,
+				  cancelButtonText: '<a href="https://jailbreak-update.github.io/" style="text-decoration: none; color: white;">No</a>', 
+				  backdrop: true, 
+				  html: '<h2>Seleccionaste idioma Español es correcto</h2>',
+				})
+			  } 
+			  if (value === 'Ingles') {
+				Swal.fire({
+				  allowOutsideClick: false,
+					 allowEscapeKey: false,
+					allowEnterKey: false,
+					showConfirmButton: true,
+					confirmButtonText: '<a href="https://jailbreak-update.github.io/en/mentenimiento.html/" style="text-decoration: none; color: white;">Yes</a>', 
+					showCancelButton: true,
+					cancelButtonText: '<a href="https://jailbreak-update.github.io/" style="text-decoration: none; color: white;">No</a>',
+					backdrop: true,
+				  html: '<h2>You selected language English is correct</h2>',
+				})
+			  } else {
+				resolve('Necesitas seleccionar un idioma')
+			  }
+			  
+			})
+		  }
+	})
+})
