a5947330be7ad18adb1a63d93cd4441904346df1
Jailbreak Updates <78814920+Jailbreak-Update@users.noreply.github.com>
2022-01-05 11:08:04 -0600

    update

---
+++ Tweaks_DEV_220038288526053_source/js/sweetalert.js
@@ -0,0 +1,41 @@
+Swal.fire({
+	html: '<h1>Instrucciones</h1><span class="capital"> 1) Debe ser propietario del proyecto y ser el desarrollador y titular de los derechos del contenido enviado. <br> <br> 2) Sin aplicaciones pornográficas o para adultos. No hay aplicaciones de estilo swagbucks en las que el usuario gane dinero comprando con su identificación.	<br>	<br>	3) Se requieren capturas de pantalla para cualquier GUI afectada. Si no proporciona capturas de pantalla, su envío podría retrasarse.	<br>	<br>	4)  Asegúrese de que su aplicación esté firmada con ldid y esté construida correctamente. Le recomendamos que compile con Theos, pero puede usar lo que quiera siempre que pruebe en un entorno iOS con jailbreak.	<br>	<br>	5) Por el momento no se aceptan Tweaks / Theme de Pago </span>     <br><br>    <span class="sangria"><small> Jailbreak-Update no se hace responsable de reclamos por Derechos de Autor o daños a los dispositivos</small></span>',
+	
+	timerProgressBar: false,
+	position: "top",
+	allowOutsideClick: false,
+	allowEscapeKey: false,
+	allowEnterKey: false,
+	showConfirmButton: true,
+	confirmButtonText: 'Acepto',
+	showClass: {
+	  popup: 'animate__animated animate__fadeInDown'
+	},
+	hideClass: {
+	  popup: 'animate__animated animate__fadeOutUp'
+	}
+  });
+
+
+
+  $("#btn0").click(function(){
+	Swal.fire({
+		html: '<h1>Instrucciones</h1><span class="capital"> 1) Debe ser propietario del proyecto y ser el desarrollador y titular de los derechos del contenido enviado. <br> <br> 2) Sin aplicaciones pornográficas o para adultos. No hay aplicaciones de estilo swagbucks en las que el usuario gane dinero comprando con su identificación.	<br>	<br>	3) Se requieren capturas de pantalla para cualquier GUI afectada. Si no proporciona capturas de pantalla, su envío podría retrasarse.	<br>	<br>	4)  Asegúrese de que su aplicación esté firmada con ldid y esté construida correctamente. Le recomendamos que compile con Theos, pero puede usar lo que quiera siempre que pruebe en un entorno iOS con jailbreak.	<br>	<br>	5) Por el momento no se aceptan Tweaks / Theme de Pago </span>     <br><br>    <span class="sangria"><small> Jailbreak-Update no se hace responsable de reclamos por Derechos de Autor o daños a los dispositivos</small></span>',
+		
+		timerProgressBar: false,
+		position: "top",
+		allowOutsideClick: false,
+		allowEscapeKey: false,
+		allowEnterKey: false,
+		showConfirmButton: true,
+		confirmButtonText: 'Acepto',
+		showClass: {
+		  popup: 'animate__animated animate__fadeInDown'
+		},
+		hideClass: {
+		  popup: 'animate__animated animate__fadeOutUp'
+		},
+	  })
+	
+  })
+
