92b54c2c0424e87b1759a7947aefa9172bef706a
AriRubDam <arirub08@gmail.com>
2022-05-14 19:42:27 -0500

    update 2

--- Buscador de dispositivos/js/sweetalert.js
+++ Buscador de dispositivos/js/sweetalert.js
@@ -1,5 +1,5 @@
 Swal.fire({
-	html:'<center><big><strong>PON EL NOMBRE DE TU DISPOSITIVO EN EL BUSCADOR QUE ESTA EN LA PARTE DE ARRIBA</strong></big></center><p><CENTER><BIG>SI TU DISPOSITIVO NO SE ENCUENTRA FAVOR DE IR A SUGERENCIAS</BIG></CENTER></P><p></P><P></P><P><H1></H1></P><CENTER><a href="/index.html" title="Pagina de inicio de Jailbreak-Update"><img src="/img/logo.png"style="border-radius: 20%;"class="bg-transparent h-10 w-10 overflow-hidden"></a></CENTER>',
+	html:'<center><big><strong>PON EL NOMBRE DE TU DISPOSITIVO EN EL BUSCADOR</strong></big></center><p><CENTER><BIG>SI TU DISPOSITIVO NO SE ENCUENTRA FAVOR DE IR A SUGERENCIAS</BIG></CENTER></P><p></P><P></P><P><H1></H1></P><CENTER><a href="/index.html" title="Pagina de inicio de Jailbreak-Update"><img src="/img/logo.png"style="border-radius: 20%;"class="bg-transparent h-10 w-10 overflow-hidden"></a></CENTER>',
 	position: "top",
 	allowOutsideClick: false,
 	allowEscapeKey: false,
