dfb9f56ae9e239b8154a4b5ed62a9160b9f52305
AriRubDam <arirub08@gmail.com>
2022-07-21 14:58:17 -0500

    Update 503.php

--- 503.php
+++ 503.php
@@ -3,5 +3,5 @@ $protocol = "HTTP/1.0";
 if ( "HTTP/1.1" == $_SERVER["SERVER_PROTOCOL"] )
 $protocol = "HTTP/1.1";
 header( "$protocol 503 Service Unavailable", true, 503 );
-header( "Retry-After: 30-07-2018 00:03" );  
+header( "Retry-After: 13-08-2022 10:00" );  
 ?>
